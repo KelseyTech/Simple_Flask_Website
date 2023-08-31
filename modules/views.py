085d5bc929b3c7df39474ddc925a3e5be52fc6df
KelseyLyrix <kelsey.paton2@gmail.com>
2023-08-31 01:30:18 +0100

    Added User post numbers

--- modules/views.py
+++ modules/views.py
@@ -1,13 +1,5 @@
 # Routes for users not logged in
-from flask import (
-    Blueprint,
-    render_template,
-    render_template_string,
-    request,
-    flash,
-    redirect,
-    url_for,
-)
+from flask import (Blueprint, render_template, render_template_string, request, flash,redirect, url_for)
 from flask_login import login_required, current_user
 
 from . import db
@@ -16,9 +8,12 @@ from .models import Post, User
 views = Blueprint("views", __name__)
 
 
-# ------------------
-# ----Home-Page-----
-# ------------------
+# -------------------------
+# -------------------------
+# --------Home-Page--------
+# -------------------------
+# -------------------------
+
 @views.route("/")
 @views.route("/home")
 @login_required
@@ -27,9 +22,13 @@ def home():
     return render_template("home.html", user=current_user, posts=posts)
 
 
-# ------------------
-# ---Create-Post----
-# ------------------
+
+# -------------------------
+# -------------------------
+# ------Create-Post--------
+# -------------------------
+# -------------------------
+
 @views.route("/home", methods=["GET", "POST"])
 @login_required
 def create_post():
@@ -46,10 +45,12 @@ def create_post():
 
     return redirect(url_for("views.home", user=current_user))
 
+# -------------------------
+# -------------------------
+# -------Delete-Post-------
+# -------------------------
+# -------------------------
 
-# ------------------
-# ---Delete-Post----
-# ------------------
 @views.route("/delete-post/<id>")
 @login_required
 def delete_post(id):
@@ -63,21 +64,31 @@ def delete_post(id):
         db.session.delete(post)
         db.session.commit()
         flash("Post deleted.", category="success")
-
     return redirect(url_for("views.home"))
 
 
-# ------------------
-# --Profile--Page---
-# ------------------
+# -------------------------
+# -------------------------
+# -----Profile--Page-------
+# -------------------------
+# -------------------------
 @views.route("/user/<username>", methods=["GET", "POST"])
 @login_required
 def profile(username):
-    user = User.query.filter_by(username=username).first()
 
+    #Allows user to create a post on their profile 
+    create_post()
+
+    #Checks if user exists
+    user = User.query.filter_by(username=username).first()
     if not user:
         flash("User does not exist", category="error")
         return redirect(url_for("views.home"))
     
+    #Grabs all the users posts 
     post = Post.query.filter_by(author=user.id).all()
-    return render_template("user.html", user=current_user, username=username, posts=post)
+
+    #Counts the number of posts the user has
+    post_number = len(post)
+
+    return render_template("user.html", user=current_user, username=username, posts=post, post_number=post_number)
